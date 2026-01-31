48593edb4fe3cc95a69265979ce67f8d9da27016
skuaej <sumankumar821310@gmail.com>
2026-01-31 12:28:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "20288951"
 api_hash = "e8cb5fb7a475b5f5eb3b0ef0e6ca03a8"
-bot_token = "8527574004:AAGOO7hahX6C6uoJ6k8rExAehpNfZyK-7bc"
+bot_token = "8527574004:AAGOO7hahX6C6yhyuoJ6k8rExAehpNfZyK-7bc"
 auth_users = [6804892450]
